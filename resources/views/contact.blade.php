3fdf7224c5d6b104e4b3c3155e4457ce53ac0c05
asleepJJ <jeraldjaybuban16@gmail.com>
2024-11-13 23:06:47 +0800

    Updated Code

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="css/contact.css">
+    <title>Contact Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/contact.css">
 </head>
 <body>
 
 <h1>CONTACT US</h1>
 <main class="container">
-
     <div class="contact-box">
         <p>We'd Love To Hear From You! <br> Let's Get in Touch</p>
         
@@ -45,10 +44,10 @@
                 <h2>Your Message</h2>
                 <textarea name="message" placeholder="Type your message here" rows="5"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary w-100">Send Message</button>
+            <button type="submit" class="btn btn-primary">Send Message</button>
         </form>
     </div>
-
+    <img src="" alt="Illustration" class="contact-image">
 </main>
 </body>
 </html>
