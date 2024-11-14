b80b26b652f94f959eaf99e35922b26fdc6e1dc0
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-11-14 21:47:12 +0800

    fixed code in sign up

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,18 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
     <link rel="stylesheet" href="css/signup.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-    <link href="https://fonts.cdnfonts.com/css/helvetica-neue-5" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=McLaren&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=McLaren&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -54,13 +52,15 @@
                     <img src="{{ asset('img/login/x.svg') }}" alt="Profile" class="x-logo"></i>
                 </a>
             </div>
+
             <div class="row">
                 <div class="left-image-container">
-                    <img src="{{ asset('img/login/front.svg') }}" alt="Profile" class="left-image">
+                    <img src="{{ asset('img/login/front.svg') }}" alt="Profile" class="img-fluid">
                 </div>
             </div>
-            <p class="acc">Already have an account?<a href="/login" style="text-decoration: none"> Login</a></p>
         </div>
+
+        <p class="acc">Already have an account?<a class="acc" href="/login" style="text-decoration: none"> Login</a></p>
     </div>
 
     <script>
