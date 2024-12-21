a43cf2f2941e12c7f8be2f063110914b5cb941cc
Laiza <hediphonextra@gmail.com>
2024-12-21 10:49:03 +0800

    Update delete-book.blade.php

--- resources/views/modals/delete-book.blade.php
+++ resources/views/modals/delete-book.blade.php
@@ -10,7 +10,7 @@
 
 <body>
     <section id="deleteBook">
-        <div id="deleteBookModal">
+        <div id="deleteBookModal" class="deleteBookModal">
             <div class="background">
                 <div class="deleteItemContainer">
                     <div class="image">
@@ -22,7 +22,7 @@
                     <div class="formContent">
                         <div class="deleteButtons">
                             <button class="deleteBook" id="deleteBtn">Confirm</button>
-                            <button class="cancel" id="deleteCancelBtn">Cancel</button>
+                            <button class="cancel" id="deleteCancelBtn" onclick="closeDeleteBookModal()">Cancel</button>
                         </div>
                     </div>
                 </div>
@@ -30,5 +30,67 @@
         </div>
     </section>
 </body>
+
+<script>
+    function showDeleteBookModal() {
+        document.getElementById('deleteBookModal').style.display = 'block';
+    }
+    
+    function closeDeleteBookModal() {
+        // Close the modal or reset the form
+        document.getElementById("deleteBookModal").style.display = "none";
+    }
+
+    //get the selected checkboxes
+    function getSelectedCheckboxes() {
+        var checkboxes = document.querySelectorAll('.inventoryTable tbody input[type="checkbox"]:checked');
+        var selectedBooks = [];
+        checkboxes.forEach(function (checkbox) {
+            selectedBooks.push(checkbox.value);
+        });
+        return selectedBooks;
+    }
+
+    //removal of selected products
+    function removeSelectedProducts() {
+        var selectedBooks = getSelectedCheckboxes();
+
+        var modalConfirm = document.getElementById("deleteBookModal");
+        modalConfirm.style.display = "flex";
+
+
+        var confirmCancelBtn = document.getElementById("confirmCancelBtn");
+
+        confirmCancelBtn.addEventListener("click", function () {
+            var modalConfirm = document.getElementById("deleteBookModal");
+
+            modalConfirm.style.display = "none";
+        });
+
+
+        var confirmation = document.getElementById("confirmBtn");
+
+        confirmation.addEventListener("click", function () {
+            //update the hidden input field with selected products 
+            document.getElementById("selectedBooks").value = selectedBooks.join(',');
+
+            document.getElementById("removeBookForm").submit();
+
+            var remove_successPrompt = document.getElementById("remove_successPrompt");
+
+            remove_successPrompt.style.display = "flex";
+
+            var remove_okBtn = document.getElementById("remove_okBtn");
+
+            remove_okBtn.addEventListener("click", function () {
+                remove_successPrompt.style.display = "none";
+            });
+        });
+    }
+
+    document.getElementById("removeBookBtn").onclick = function () {
+        removeSelectedProducts();
+    };
+</script>
 
 </html>
