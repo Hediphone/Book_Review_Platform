d4e99645ae9c5f3244c109f9c9b9f83893e5c2e5
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-12-23 06:08:09 +0800

    login adjustments

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
-    <!-- CSS Links -->
     <link rel="stylesheet" href="assets/css/login.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/helvetica-neue-5" rel="stylesheet">
@@ -16,29 +14,28 @@
     <link href="https://fonts.googleapis.com/css2?family=McLaren&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="dashboard_container">
         <div class="row">
-            <p class="login">Welcome Back</p>
+            <p class="login">Welcome Back!</p>
             <p class="sign">Login to Continue</p>
 
-            <!-- Session messages -->
             @if(session()->has("success"))
                 <div class="alert alert-success">
                     {{ session()->get("success") }}
                 </div>
             @endif
             @if(session()->has("error"))
-                <div class="alert alert-danger">
-                    {{ session()->get("error") }}
-                </div>
+                    <div class="alert alert-success">
+                        {{ session()->get("error") }}
+                    </div>
             @endif
-
-            <!-- Login Form -->
-            <form class="login_form" method="POST" action="{{ route('login.post') }}">
+           
+            <form class="login_form" method="POST" action="{{route("login.post")}}">
                 @csrf
                 <div class="input_container">
                     <i class="fas fa-user icon"></i>
@@ -57,12 +54,9 @@
                 <button type="submit" class="login_button">Login</button>
             </form>
 
-            <!-- Forgot Password Link -->
             <a class="forgot" href="#">Forgot Password</a>
-
             <p class="login1">Login with</p>
 
-            <!-- Social Icons -->
             <div class="social-icons text-center mt-3">
                 <a href="https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-920004606%3A1731464646265211&ddm=1" class="mx-3" style="text-decoration: none" target="_blank">
                     <i class="fab fa-google fa-2x"></i>
@@ -71,39 +65,33 @@
                     <i class="fab fa-facebook fa-2x"></i>
                 </a>
                 <a href="#" class="mx-3">
-                    <img src="{{ asset('assets/images/login/x.svg') }}" alt="Profile" class="x-logo">
+                    <img src="{{ asset('assets/images/login/x.svg') }}" alt="Profile" class="x-logo"></i>
                 </a>
             </div>
-
-            <!-- Image Section -->
             <div class="row">
                 <div class="left-image-container">
                     <img src="{{ asset('assets/images/login/front.svg') }}" alt="Profile" class="left-image">
                 </div>
             </div>
-
-            <!-- Sign Up Link -->
-            <p class="acc">New User?<a href="{{ route('register') }}" style="text-decoration: none"> Sign Up</a></p>
+            <p class="acc">New User?<a href="{{route("register")}}" style="text-decoration: none"> Sign Up</a></p>
         </div>
     </div>
 
-    <!-- Toggle Password Visibility Script -->
     <script>
         function togglePassword() {
             const passwordField = document.querySelector('.password_field');
             const toggleIcon = document.querySelector('.toggle_password');
     
-            if (passwordField.type === 'password') {
-                passwordField.type = 'text';
-                toggleIcon.classList.remove('fa-eye');
-                toggleIcon.classList.add('fa-eye-slash');
-            } else {
-                passwordField.type = 'password';
-                toggleIcon.classList.remove('fa-eye-slash');
-                toggleIcon.classList.add('fa-eye');
+                if (passwordField.type === 'password') {
+                    passwordField.type = 'text';
+                    toggleIcon.classList.remove('fa-eye');
+                    toggleIcon.classList.add('fa-eye-slash');
+                } else {
+                    passwordField.type = 'password';
+                    toggleIcon.classList.remove('fa-eye-slash');
+                    toggleIcon.classList.add('fa-eye');
+                }
             }
-        }
     </script>
-</body>
 
-</html>
+</body>
