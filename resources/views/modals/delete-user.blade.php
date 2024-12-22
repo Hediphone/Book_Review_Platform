ee8f23897ff827f70249b1adb68f8916b2c70156
Laiza <hediphonextra@gmail.com>
2024-12-23 00:09:13 +0800

    centered the modal

--- resources/views/modals/delete-user.blade.php
+++ resources/views/modals/delete-user.blade.php
@@ -1,13 +1,15 @@
 @if(session('deleteUser'))
     <section id="DeleteUserSuccess">
-        <div class="successPrompt" id="successUserDeleteModal" style="display:flex;">
-            <div class="sucessContainer">
-                <div class="successMessage">
-                    <p>{{ session('message') }}</p>
-                </div>
-                <div class="formContent">
-                    <div class="successmodalButtons">
-                        <button class="addBook" id="add_okBtn" onclick="closeSuccessUserDeleteModal()">Ok</button>
+        <div class="deleteUsersModal" id="successUserDeleteModal" style="display:block;">
+            <div class="background">
+                <div class="deleteItemContainer">
+                    <div class="message">
+                        <p>User successfully deleted.</p>
+                    </div>
+                    <div class="formContent">
+                        <div class="deleteButtons">
+                            <button class="deleteBook" id="okDeleteBtn" onclick="closeSuccessUserDeleteModal()">Ok</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -17,15 +19,17 @@
 
 <!-- Confirmation Modal -->
 <section id="DeleteUserConfirmation">
-    <div class="successPrompt" id="confirmUserDeleteModal" style="display:none;">
-        <div class="sucessContainer">
-            <div class="successMessage">
-                <p>Are you sure you want to delete this user?</p>
-            </div>
-            <div class="formContent">
-                <div class="successmodalButtons">
-                    <button class="addBook" id="deleteUserBtn">Yes</button>
-                    <button class="addBook" id="cancelUserDeleteBtn">No</button>
+    <div class="deleteUsersModal" id="confirmUserDeleteModal" style="display:none;">
+        <div class="background">
+            <div class="deleteItemContainer">
+                <div class="message">
+                    <p>Are you sure you want to delete this user?</p>
+                </div>
+                <div class="formContent">
+                    <div class="deleteButtons">
+                        <button class="deleteBook" id="deleteUserBtn">Yes</button>
+                        <button class="cancel" id="cancelUserDeleteBtn">No</button>
+                    </div>
                 </div>
             </div>
         </div>
